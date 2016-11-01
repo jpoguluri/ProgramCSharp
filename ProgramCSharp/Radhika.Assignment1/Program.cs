13e74f9aba04f2f2c0db05437da1c04194ba5075
Radhika <radhikakoganti@gmail.com>
2016-10-31 21:00:56 -0700

    Bible Test application

--- ProgramCSharp/Radhika.Assignment1/Program.cs
+++ ProgramCSharp/Radhika.Assignment1/Program.cs
@@ -10,6 +10,16 @@ namespace Radhika.Assignment1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number for tables");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Table of {0}", a);
+            for (int i = 1; i > 11; i++)
+            {
+                Console.WriteLine(" {0}*{a}", a * i);
+                Console.ReadLine();
+            }
+          
+     
         }
     }
 }
