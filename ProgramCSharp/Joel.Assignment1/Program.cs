3d66fd31054cf3f61e0f92c4a9739b34b11ed687
Joel <Joel@PJ>
2016-11-01 18:41:22 -0700

    Pushing for condition assignment

--- ProgramCSharp/Joel.Assignment1/Program.cs
+++ ProgramCSharp/Joel.Assignment1/Program.cs
@@ -10,9 +10,24 @@ namespace Joel.Assignment1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Sum of  {0}+{1} is {2}", 3, 4, (3 + 4));
+           Console.WriteLine("Difference of {0}-{1}, is {2}", 7, 2, (7 - 2));
+           Console.WriteLine("Product of {0}*{1} is {2}", 2, 8, (8 * 2));
 
-            Console.WriteLine("Praise the LORD");
+            Console.WriteLine("7th Table");
+            
+
+            int i = 1;
+
+            for  (i = 1; i <= 70; i++)
+            {
+                if (i % 7 == 0)
+               {
+                    Console.WriteLine(i);
+                }
+            }
             Console.ReadLine();
+
         }
     }
 }
