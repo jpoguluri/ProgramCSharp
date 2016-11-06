e13292d761be1d1352e84764fb715ed93535b57e
Radhika <radhikakoganti@gmail.com>
2016-11-05 18:01:13 -0700

    Assignment2 changed

---
+++ ProgramCSharp/Radhika.Assignment2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radhika.Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The String :");
+            string s = Console.ReadLine();
+            int count = 0;
+            string[] substrings = s.Split(' ');
+            foreach( string str in substrings)
+            {
+                string temp = string.Empty;
+                for (int i = str.Length - 1; i >= 0; i--)
+                {
+
+                    temp = temp + str[i];
+                }
+                if (str== temp)
+                {
+                    Console.WriteLine("{0} is Palidrome ", str);
+                    count++;
+                }
+             }
+            Console.WriteLine("{0} Palidrome(s) in the String", count);
+            Console.ReadLine();
+         }
+    }
+}
