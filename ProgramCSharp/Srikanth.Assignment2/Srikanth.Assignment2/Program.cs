c62792aaf9480f15fad19841098d3ea6809634fa
Srikanth Reddy Thumma <mastersrikanth@rediffmail.com>
2016-11-05 23:45:05 -0500

    sri

--- ProgramCSharp/Srikanth.Assignment2/Srikanth.Assignment2/Program.cs
+++ ProgramCSharp/Srikanth.Assignment2/Srikanth.Assignment2/Program.cs
@@ -10,6 +10,67 @@ namespace Srikanth.Assignment2
     {
         static void Main(string[] args)
         {
+            string[] arrayOfStrings = new string[5];
+
+            int totalNumOfPalindromes = 0;
+            string stringOfPalindromes = null;
+            int count = 0;
+
+            //string palindromeString = null;
+            for (int i = 0; i < arrayOfStrings.Length; i++)
+            {
+                Console.WriteLine("Please enter {0}th element of arrayOfStrings", i);
+                arrayOfStrings[i] = Console.ReadLine();
+            }
+            foreach (string word in arrayOfStrings)
+            {
+                Console.WriteLine(word);
+            }
+
+            for (int j = 0; j < arrayOfStrings.Length; j++)
+            {
+                var modifiedString = arrayOfStrings[j];
+                modifiedString = modifiedString.Replace(" ", "");
+                arrayOfStrings[j] = modifiedString;
+
+                char[] stringCharArray = arrayOfStrings[j].ToCharArray();
+                for (int k = 0; k < stringCharArray.Length; k++)
+                {
+                    count = 0;
+
+                    if (stringCharArray[k] == stringCharArray[(((stringCharArray.Length) - 1) - k)])
+                    {
+
+                        count = count + 1;
+
+                    }
+                    else
+                    {
+                        count = 0;
+                        break;
+
+                    }
+
+                }
+                if (count > 0)
+                {
+                    totalNumOfPalindromes = totalNumOfPalindromes + 1;
+                    stringOfPalindromes = arrayOfStrings[j] + "," + stringOfPalindromes;
+                }
+
+            }
+
+
+            Console.WriteLine("The number of Palindrome strings are {0}", totalNumOfPalindromes);
+            Console.WriteLine(stringOfPalindromes);
+            Console.ReadLine();
+
+
+
         }
     }
 }
+
+
+
+
